e6b63f1de2bc263dad2fe7a1c903dede425ea4f6
Murilo Bada <murilomaffiolettibada@hotmail.com>
2023-04-01 17:22:36 -0300

    Commit

--- client/cl_game.lua
+++ client/cl_game.lua
@@ -5,7 +5,7 @@ if Config.AnonymousNames then
             Wait(5000)
             for _, player in ipairs(GetActivePlayers()) do
                 local ped = GetPlayerPed(player)
-                SetPedPromptName(ped, "Stranger (" .. tostring(GetPlayerServerId(player))..")")
+                SetPedPromptName(ped, tostring(GetPlayerServerId(player)))
             end
         end
     end)
